84e1bfe08b60fa89304c1cda94bc06940681c755
marcelo <maxcelu@hotmail.com>
2022-10-02 00:46:56 -0300

    fix style2

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ const showMenu = document.querySelector('#menu-popup')
 const closeMenu = document.querySelector('#menu-close')
 const barraLateral = document. querySelector('.barra-lateral')
 showMenu.addEventListener('click', ()=>{
+    showMenu.style.display = "none"
     barraLateral.classList.remove("closedMenu")
     barraLateral.classList.add("menu-show")
 })
 closeMenu.addEventListener('click', ()=>{
     barraLateral.classList.remove("menu-show")
     barraLateral.classList.add("closedMenu")
+    setTimeout(() => {
+        showMenu.style.display = "block"
+    }, 250);
 })
