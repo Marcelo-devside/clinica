44debb99b000a2c17bc0a7cf893b406c616ed005
marcelo <maxcelu@hotmail.com>
2022-11-02 21:56:57 -0300

    fix date happy birthday

--- Cadastro/Acadastro/script.js
+++ Cadastro/Acadastro/script.js
@@ -35,23 +35,52 @@ cpfId.addEventListener("click", ()=>{
     labelCpf.classList.replace('label-cpf','genericLabelIsLater')
     labelCpf.innerHTML=`<label>DNV</label>`
 })
+labelCpf.addEventListener("click", ()=>{
+    labelCpf.classList.replace('label-cpf','genericLabelIsLater')
+    labelCpf.innerHTML=`<label>DNV</label>`
+    cpfId.focus()
+})
 bornDate.addEventListener("click", ()=>{
     labelBorn.classList.replace('label-born','genericLabelIsLater')
+})
+labelBorn.addEventListener("click", ()=>{
+    labelBorn.classList.replace('label-born','genericLabelIsLater')
+    bornDate.focus()
 })
 resildencialInput.addEventListener("click", ()=>{
     labelResildencial.classList.replace('label_residencial','genericLabelIsLater')
 })
+labelResildencial.addEventListener("click", ()=>{
+    labelResildencial.classList.replace('label_residencial','genericLabelIsLater')
+    resildencialInput.focus()
+})
 celularInput.addEventListener("click", ()=>{
     labelCelular.classList.replace('label_celular','genericLabelIsLater')
+})
+labelCelular.addEventListener("click", ()=>{
+    labelCelular.classList.replace('label_celular','genericLabelIsLater')
+    celularInput.focus()
 })
 patientInput.addEventListener("click", ()=>{
     labelPatient.classList.replace('label_patient','genericLabelIsLater')
 })
+labelPatient.addEventListener("click", ()=>{
+    labelPatient.classList.replace('label_patient','genericLabelIsLater')
+    patientInput.focus()
+})
 motherInput.addEventListener("click", ()=>{
     labelMother.classList.replace('label_mather','genericLabelIsLater')
 })
+labelMother.addEventListener("click", ()=>{
+    labelMother.classList.replace('label_mather','genericLabelIsLater')
+    motherInput.focus()
+})
 fatherInput.addEventListener("click", ()=>{
     labelFather.classList.replace('label_father','genericLabelIsLater')
+})
+labelFather.addEventListener("click", ()=>{
+    labelFather.classList.replace('label_father','genericLabelIsLater')
+    fatherInput.focus()
 })
 cpfId.addEventListener("focusout", (e)=>{
     valor = e.target.value;
@@ -70,29 +99,117 @@ bornDate.addEventListener("focusout", (e)=>{
         labelBorn.classList.replace('genericLabelIsLater','label-born')
     } else {
         labelBorn.classList.replace('label-born','genericLabelIsLater')
+        Idade()
     }
 })
+
 function Idade(){
     let today = new Date();
+    console.log("today", today)
     const data_atual = new Intl.DateTimeFormat(['bra', 'id']).format(today);
     // const transfomrDate = data_atual.toString();
     let data_atualFormat = data_atual.replace(/[/]/g, "");
-    let ano_atual = data_atualFormat.substring(4, 8)
+    let ano_atual = data_atualFormat.substring(3, 8)
     console.log("ano atual", ano_atual)
-    let mes_atual = data_atualFormat.substring(2, 4)
+    let mes_atual = data_atualFormat.substring(1, 3)
     console.log("mês atual", mes_atual)
-    let dia_atual = data_atualFormat.substring(0, 2)
+    let dia_atual = data_atualFormat.substring(0, 1)
     console.log("dia atual", dia_atual)
-    let nascimento = bornDate.value
+    let nascimento = document.querySelector('.happyBirthday').value
+    console.log("data_atual", data_atual)
     let nascimentoFormat = nascimento.replace(/[/]/g, "");
+    console.log("data_atualFormat", data_atualFormat)
     let ano_nascimento = nascimentoFormat.substring(4, 8)
     console.log("ano do nascimento", ano_nascimento)
     let mes_nascimento = nascimentoFormat.substring(2, 4)
     console.log("mês do nascimento", mes_nascimento)
     let dia_nascimento = nascimentoFormat.substring(0, 2)
     console.log("dia do nascimento", dia_nascimento)
-}
-console.log(Idade())
+    function anoBissexto(ano_nascimento){
+            if(ano_nascimento % 4  == 0 && ano_nascimento % 100 != 0 || ano_nascimento % 400 == 0){
+                console.log("É bissexto!")
+                return 1
+            } else {
+                console.log("Não é bissexto!")
+                return 0
+        }
+    }
+    anoBissexto(ano_nascimento)
+
+    function validaData(dia, mes, ano){
+        if(dia == 29 && mes == 02 && anoBissexto(ano) == 0 ){
+            return 0
+        } else if(dia > 29 && mes == 02){
+            return 0
+        } else if(dia < 01 || dia > 31 || mes < 01 || mes > 12){
+            return 0
+        } else if(dia == 31 && (mes == 04 || mes == 06 || mes == 09 || mes == 11)){
+            return 0
+        } else {
+            return 1
+        }
+    }
+    validaData(dia_nascimento, mes_nascimento, ano_nascimento)
+
+    function diasDoMes(m, a){
+            const mes1 = ["01", "03", "05", "07", "08", "10", "12"]
+            const mes2 = ["04", "06", "09", "11"]
+            if (mes1.includes(m)){
+                console.log("Mês tem 31 dias")
+                return 31
+            } else if(mes2.includes(m)){
+                console.log("Mês tem 30 dias")
+                return 30
+            } else if(m == 02 && anoBissexto(a) == 1){
+                console.log("Mês tem 29 dias")
+                return 29
+            } else {
+                console.log("Mês tem 28 dias")
+                return 28
+            }
+        }
+    diasDoMes(mes_nascimento, ano_nascimento)
+        function calculandoIdade(dia_nascimento, mes_nascimento, ano_nascimento, dia_atual, mes_atual, ano_atual){
+            let qtdDias = 0
+            let qtdMes = 0
+            let qtdAno = 0
+            while(mes_nascimento < mes_atual  || ano_nascimento < ano_atual){
+                mes_nascimento++
+                qtdMes++
+                if(qtdMes == 12){
+                    qtdMes = 0
+                    qtdAno++
+                }
+                if(mes_nascimento == 12){
+                    ano_nascimento++
+                    mes_nascimento = 0
+                }
+            }
+            if(dia_nascimento == dia_atual){
+                qtdMes++
+                qtdDias = 0
+            } else if(dia_nascimento < dia_atual){
+                qtdMes++
+                qtdDias = dia_nascimento - dia_atual
+            } else {
+                qtdDias = diasDoMes(mes_atual , ano_atual) + dia_atual - dia_nascimento
+            }
+            // debugger
+            console.log(`${qtdAno} anos, ${qtdMes} meses e ${qtdDias} dias`)
+        }
+        if(validaData(dia_nascimento, mes_nascimento, ano_nascimento) == 1 ){
+        calculandoIdade(dia_nascimento, mes_nascimento, ano_nascimento, dia_atual, mes_atual, ano_atual)
+        } else {
+            console.log("DATA INVALIDA!")
+        }
+    }
+
+//     if (mes_nascimento > mes_atual){
+//         console.log((ano_atual - ano_nascimento) -1 )
+//     } else {
+//         console.log(ano_atual - ano_nascimento)
+//     }
+// }
 resildencialInput.addEventListener("focusout", (e)=>{
     valor = e.target.value;
     if (valor === "") {
@@ -218,12 +335,12 @@ cpfId.addEventListener("keypress", (e) => {
         e.preventDefault();
     }
 });
-let entr = document.querySelector(".form-control").value 
-entr.addEventListener('keypress', function(e){
-    if (!checkInput(e)){
-        e.preventDefault();
-    }
-});
+// let entr = document.querySelector(".form-control").value 
+// entr.addEventListener('keypress', function(e){
+//     if (!checkInput(e)){
+//         e.preventDefault();
+//     }
+// });
 
 function checkInput(e){
     const char = String.fromCharCode(e.keyCode);
